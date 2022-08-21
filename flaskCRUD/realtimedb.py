49a66ccc9c199ef04b25ad89fe78cfebe98c1b4e
chalitha-pramod <73438443+chalitha-pramod@users.noreply.github.com>
2022-08-21 21:18:46 +0530

    Add files via upload

---
+++ flaskCRUD/realtimedb.py
@@ -0,0 +1,13 @@
+import pyrebase
+
+
+
+config = {
+    "apiKey": "AIzaSyBHIUC_82YEZFONkxCpbQQt4QGm_B86_gA",
+    "authDomain": "crudapp-4742d.firebaseapp.com",
+    "projectId": "crudapp-4742d",
+    "storageBucket": "crudapp-4742d.appspot.com",
+    "messagingSenderId": "313127648378",
+    "appId": "1:313127648378:web:d000c333f833f57336cfc3",
+    "measurementId": "G-S7DJ12K3MK"
+  };
